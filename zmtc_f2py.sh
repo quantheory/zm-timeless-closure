302835c0763052f4a3432a6aeb7fb89e6fa85e67
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2020-12-29 16:36:50 -0600

    Floating-point trapping doesn't work in python tests.

--- zmtc_f2py.sh
+++ zmtc_f2py.sh
@@ -3,7 +3,7 @@
 f90flags=
 if [ $1 = "-t" ]; then
    # Note: gfortran-specific check flag.
-   f90flags="$fflags -DTEST_MODE -fcheck=all -ffpe-trap=invalid,zero,overflow"
+   f90flags="$fflags -DTEST_MODE -fcheck=all"
 fi
 
 python -m numpy.f2py -c --f2cmap .f2py_f2cmap -m fort_zmtc --f90flags="$f90flags" shr_kind_mod.F90 zm_timeless_closure.F90
